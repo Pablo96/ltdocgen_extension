39d0218ee5ea5ec966b7ca1400acc4b464ff57b0
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-06-14 20:43:53 -0300

    Snippet and corrected @ trigger

--- src/extension.ts
+++ src/extension.ts
@@ -15,9 +15,11 @@ enum CommentkeyWords {
 	BRIEF
 };
 
-let commentKeyWords: string[] = [
+const commentKeyWords: string[] = [
 	"variable ", "enum ", "struct ", "union ", "field ", "func ", "param ", "file ", "author ", "brief "
 ];
+
+const triggerSequence: string = "/**";
 
 class CommentAutocompleter implements vscode.CompletionItemProvider {
 	firstStar: boolean = true;
@@ -26,51 +28,103 @@ class CommentAutocompleter implements vscode.CompletionItemProvider {
 		token: vscode.CancellationToken, context: vscode.CompletionContext)
 	: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
 	{
-		if (context.triggerCharacter === '@') {
-			return [
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]),
-				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
-			];
-		} else if (context.triggerCharacter === '*') {
-			if (this.firstStar === true) {
-				this.firstStar = false;
-				return null;
-			}
-			
-			this.firstStar = true;
-
-			let textEditor = vscode.window.activeTextEditor;
-			let fileName: string = textEditor === undefined ? "fileName" : textEditor.document.fileName;
-			
-			const fileDescrition: string = 
-				"\n* @" + commentKeyWords[CommentkeyWords.FILE] + fileName +
-				"\n* @" + commentKeyWords[CommentkeyWords.AUTHOR] + "John Doe" + 
-				"\n* @" + commentKeyWords[CommentkeyWords.BRIEF] + " This file contains code about something." +
-				"\n* */";
-				
-			const defaultDocComment: string =
-				 "\n* @" + commentKeyWords[CommentkeyWords.STRUCT] + "structName" + 
-				 "\n* @" + commentKeyWords[CommentkeyWords.FIELD] + "fieldName:" + 
-				 "\n*\t@" + commentKeyWords[CommentkeyWords.BRIEF] + "this field describe this thing." +
-				 "\n* */";
-			return [
-				new vscode.CompletionItem(fileDescrition),
-				new vscode.CompletionItem(defaultDocComment)
-			];
-		} else if (context.triggerCharacter === '/') {
-			return null;
-		} else {
+		if (context.triggerCharacter !== '@') {
 			return null;
 		}
+		
+		return [
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
+		];
 	}
+}
+
+function inComment(activeEditor: vscode.TextEditor, activeLine: number): boolean {
+	if (activeLine === 0) {
+		return false;
+	}
+
+	const txt: string = activeEditor.document.lineAt(activeLine - 1).text.trim();
+	if (txt.startsWith("///") && txt.startsWith("*") &&
+		txt.startsWith("/**") && txt.startsWith("/*!")) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function check(activeEditor: vscode.TextEditor, event: vscode.TextDocumentContentChangeEvent): boolean {
+	if (activeEditor === undefined || activeEditor === null ||
+		event === undefined || event.text === null) {
+		return false;
+	}
+	const activeSelection: vscode.Position = activeEditor.selection.active;
+	const activeLine: vscode.TextLine = activeEditor.document.lineAt(activeSelection.line);
+	const activeChar: string = activeLine.text.charAt(activeSelection.character);
+	const startsWith: boolean = event.text.startsWith("\n") || event.text.startsWith("\r\n");
+
+	// Check if enter was pressed. Note the !
+	if (!((activeChar === "") && startsWith)) {
+		return false;
+	}
+
+	// Check if not currently in a comment block
+	if (!inComment(activeEditor, activeSelection.line)) {
+		return false;
+	}
+
+	// Do not trigger when there's whitespace after the trigger sequence
+	// tslint:disable-next-line:max-line-length
+	const seq = "[\\s]*(" + triggerSequence.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&") + ")$";
+	const match = activeLine.text.match(seq);
+
+	if (match !== null) {
+		const cont: string = match[1];
+		return triggerSequence === cont;
+	} else {
+		return false;
+	}
+}
+function onEvent(activeEditor: vscode.TextEditor, event: vscode.TextDocumentContentChangeEvent) {
+	const lang: string = activeEditor.document.languageId;
+
+	if (lang !== 'c') {
+		console.log("No comments can be generated for language: " + lang);
+		return null;
+	}
+
+	if (!check(activeEditor, event)) {
+		return null;
+	}
+
+	const currentPos: vscode.Position = activeEditor.selection.active;
+	const startReplace: vscode.Position = new vscode.Position(
+		currentPos.line,
+		currentPos.character - triggerSequence.length,
+	);
+
+	let textEditor = vscode.window.activeTextEditor;
+	let fileName: string = textEditor === undefined ? "fileName" : textEditor.document.fileName;
+	
+	const fileDescrition: string = 
+		"\n* @" + commentKeyWords[CommentkeyWords.FILE] + fileName +
+		"\n* @" + commentKeyWords[CommentkeyWords.AUTHOR] + "John Doe" + 
+		"\n* @" + commentKeyWords[CommentkeyWords.BRIEF] + " This file contains code about something." +
+		"\n* */";
+		
+	const defaultDocComment: string =
+		 "\n* @" + commentKeyWords[CommentkeyWords.STRUCT] + "structName" + 
+		 "\n* @" + commentKeyWords[CommentkeyWords.FIELD] + "fieldName:" + 
+		 "\n*\t@" + commentKeyWords[CommentkeyWords.BRIEF] + "this field describe this thing." +
+		 "\n* */";
 }
 
 // this method is called when your extension is activated
@@ -80,8 +134,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	vscode.window.showInformationMessage('Llamathrust DocGen extension activated!');
 
+	vscode.workspace.onDidChangeTextDocument((event) => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor && event.document === activeEditor.document) {
+			onEvent(activeEditor, event.contentChanges[0]);
+		}
+	});
+
 	// Autocompleter
-	let autocompleter = vscode.languages.registerCompletionItemProvider('c', new CommentAutocompleter(), '@', '/', '*');
+	let autocompleter = vscode.languages.registerCompletionItemProvider('c', new CommentAutocompleter(), '@');
 	context.subscriptions.push(autocompleter);
 }
 
