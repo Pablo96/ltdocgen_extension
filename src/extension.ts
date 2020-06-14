c146644b7f4b9b91417b2c7ecf48c295d23fb1cf
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-06-14 19:37:56 -0300

    Comment Autocompletion

--- src/extension.ts
+++ src/extension.ts
@@ -2,37 +2,75 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-/**
- * IntelliSense completion class
- */
-class CompletionItem implements vscode.CompletionItemProvider{
-	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position,
-				token: vscode.CancellationToken, context: vscode.CompletionContext)
-			: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
-	{
-		return [
-			new vscode.CompletionItem("\n* @struct StructExample\n* */")
-		];
-	}
-}
+enum CommentkeyWords {
+	VARIABLE,
+	ENUM,
+	STRUCT,
+	UNION,
+	FIELD,
+	FUNC,
+	PARAM,
+	AUTHOR,
+	FILE,
+	BRIEF
+};
+
+let commentKeyWords: string[] = [
+	"variable ", "enum ", "struct ", "union ", "field ", "func ", "param ", "file ", "author ", "brief "
+];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('LT DocGen is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let linter = vscode.commands.registerCommand('ltdocgen', () => {
-		// The code you place here will be executed every time your command is executed
-		vscode.window.showInformationMessage('Hello World from Llamathrust DocGen!');
-	});
+	vscode.window.showInformationMessage('Llamathrust DocGen extension activated!');
 
-	context.subscriptions.push(linter);
-
-	let sel: vscode.DocumentSelector = { scheme: 'file', language: 'c' };
-	let autocompleter = vscode.languages.registerCompletionItemProvider(sel, new CompletionItem());
+	// Autocompleter
+	let autocompleter = vscode.languages.registerCompletionItemProvider('c', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position,
+			token: vscode.CancellationToken, context: vscode.CompletionContext)
+		: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
+		{
+			if (context.triggerCharacter === '@') {
+				return [
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]),
+					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
+				];
+			} else if (context.triggerCharacter === '*') {
+				let textEditor = vscode.window.activeTextEditor;
+				let fileName: string = textEditor === undefined ? "fileName" : textEditor.document.fileName;
+				
+				const fileDescrition: string = 
+					"\n* @" + commentKeyWords[CommentkeyWords.FILE] + fileName +
+					"\n* @" + commentKeyWords[CommentkeyWords.AUTHOR] + "John Doe" + 
+					"\n* @" + commentKeyWords[CommentkeyWords.BRIEF] + " This file contains code about something." +
+					"\n* */";
+					
+				const defaultDocComment: string =
+					 "\n* @" + commentKeyWords[CommentkeyWords.STRUCT] + "structName" + 
+					 "\n* @" + commentKeyWords[CommentkeyWords.FIELD] + "fieldName:" + 
+					 "\n*\t@" + commentKeyWords[CommentkeyWords.BRIEF] + "this field describe this thing." +
+					 "\n* */";
+				return [
+					new vscode.CompletionItem(fileDescrition),
+					new vscode.CompletionItem(defaultDocComment)
+				];
+			} else if (context.triggerCharacter === '/') {
+				return null;
+			} else {
+				return null;
+			}
+		}
+	}, '@', '/', '*');
 	context.subscriptions.push(autocompleter);
 }
 
