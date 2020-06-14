7aed8d16aec2c1fe0034acbda6fc4671f5b4111c
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-06-14 19:43:30 -0300

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,60 @@ let commentKeyWords: string[] = [
 	"variable ", "enum ", "struct ", "union ", "field ", "func ", "param ", "file ", "author ", "brief "
 ];
 
+class CommentAutocompleter implements vscode.CompletionItemProvider {
+	firstStar: boolean = true;
+
+	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position,
+		token: vscode.CancellationToken, context: vscode.CompletionContext)
+	: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
+	{
+		if (context.triggerCharacter === '@') {
+			return [
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]),
+				new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
+			];
+		} else if (context.triggerCharacter === '*') {
+			if (this.firstStar === true) {
+				this.firstStar = false;
+				return null;
+			}
+			
+			this.firstStar = true;
+
+			let textEditor = vscode.window.activeTextEditor;
+			let fileName: string = textEditor === undefined ? "fileName" : textEditor.document.fileName;
+			
+			const fileDescrition: string = 
+				"\n* @" + commentKeyWords[CommentkeyWords.FILE] + fileName +
+				"\n* @" + commentKeyWords[CommentkeyWords.AUTHOR] + "John Doe" + 
+				"\n* @" + commentKeyWords[CommentkeyWords.BRIEF] + " This file contains code about something." +
+				"\n* */";
+				
+			const defaultDocComment: string =
+				 "\n* @" + commentKeyWords[CommentkeyWords.STRUCT] + "structName" + 
+				 "\n* @" + commentKeyWords[CommentkeyWords.FIELD] + "fieldName:" + 
+				 "\n*\t@" + commentKeyWords[CommentkeyWords.BRIEF] + "this field describe this thing." +
+				 "\n* */";
+			return [
+				new vscode.CompletionItem(fileDescrition),
+				new vscode.CompletionItem(defaultDocComment)
+			];
+		} else if (context.triggerCharacter === '/') {
+			return null;
+		} else {
+			return null;
+		}
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -27,50 +81,7 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage('Llamathrust DocGen extension activated!');
 
 	// Autocompleter
-	let autocompleter = vscode.languages.registerCompletionItemProvider('c', {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position,
-			token: vscode.CancellationToken, context: vscode.CompletionContext)
-		: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
-		{
-			if (context.triggerCharacter === '@') {
-				return [
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]),
-					new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
-				];
-			} else if (context.triggerCharacter === '*') {
-				let textEditor = vscode.window.activeTextEditor;
-				let fileName: string = textEditor === undefined ? "fileName" : textEditor.document.fileName;
-				
-				const fileDescrition: string = 
-					"\n* @" + commentKeyWords[CommentkeyWords.FILE] + fileName +
-					"\n* @" + commentKeyWords[CommentkeyWords.AUTHOR] + "John Doe" + 
-					"\n* @" + commentKeyWords[CommentkeyWords.BRIEF] + " This file contains code about something." +
-					"\n* */";
-					
-				const defaultDocComment: string =
-					 "\n* @" + commentKeyWords[CommentkeyWords.STRUCT] + "structName" + 
-					 "\n* @" + commentKeyWords[CommentkeyWords.FIELD] + "fieldName:" + 
-					 "\n*\t@" + commentKeyWords[CommentkeyWords.BRIEF] + "this field describe this thing." +
-					 "\n* */";
-				return [
-					new vscode.CompletionItem(fileDescrition),
-					new vscode.CompletionItem(defaultDocComment)
-				];
-			} else if (context.triggerCharacter === '/') {
-				return null;
-			} else {
-				return null;
-			}
-		}
-	}, '@', '/', '*');
+	let autocompleter = vscode.languages.registerCompletionItemProvider('c', new CommentAutocompleter(), '@', '/', '*');
 	context.subscriptions.push(autocompleter);
 }
 
