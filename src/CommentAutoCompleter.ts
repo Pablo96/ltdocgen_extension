f42e01dd689f0f895ce0aaf76becd6d32c7ef33f
Pablo Narvaja <pnarvaja.21@gmail.com>
2020-06-15 12:07:06 -0300

    Context @ suggestion

--- src/CommentAutoCompleter.ts
+++ src/CommentAutoCompleter.ts
@@ -9,52 +9,68 @@ export class CommentAutocompleter implements vscode.CompletionItemProvider {
 		token: vscode.CancellationToken, context: vscode.CompletionContext)
 	: vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>>
 	{
+		// if it wasn't  triggered by the user on purpose return
 		if (context.triggerCharacter !== '@') {
 			return null;
 		}
 
+		// if not active editor return (added just for security but not sure if needed)
 		let activeEditor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 		if (activeEditor === undefined) {
 			return null;
 		}
 
-		let addNewLine: boolean = false;
-
 		let activeLine: number = activeEditor.selection.active.line;
+
+		// If it isn't inside a comment return
 		let activeLineText = document.lineAt(activeLine).text.trim();
-		
-		if (activeLineText.startsWith(triggerSequence)) {
-			activeLine++;
-			activeLineText = document.lineAt(activeLine).text.trim();
-			if (activeLineText.endsWith('*/')) {
-				addNewLine = true;
-				activeLine--;
-				activeLineText = document.lineAt(activeLine).text.trim();
-			}
-		} else if (activeLineText.endsWith('*/')) {
-			activeLine--;
-			activeLineText = document.lineAt(activeLine).text.trim();
-			if (activeLineText.startsWith(triggerSequence)) {
-				addNewLine = true;
-			}
-		} else if (!activeLineText.startsWith('*')) {
+		if (!activeLineText.startsWith(triggerSequence)
+			&& !activeLineText.startsWith('*')) {
 			return null;
 		}
 		
-		let newLine: string = addNewLine? arroba : "";
-
-		return [
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.VARIABLE]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.ENUM]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.STRUCT]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.UNION]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.FIELD]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.FUNC]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.PARAM]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.AUTHOR]),
-			new vscode.CompletionItem(newLine + commentKeyWords[CommentkeyWords.FILE]),
-			new vscode.CompletionItem((addNewLine? arrobaTab : "") + commentKeyWords[CommentkeyWords.BRIEF]),
-			new vscode.CompletionItem((addNewLine? arrobaTab : "") + commentKeyWords[CommentkeyWords.TYPE]),
+		let items = [
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.AUTHOR]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.BRIEF]),
+			new vscode.CompletionItem(commentKeyWords[CommentkeyWords.NOTE])
 		];
+
+		let beforeLine = --activeLine;
+		let lineBeforeText: string = document.lineAt(beforeLine).text.trim();
+		console.log('LineBefore (number: ' + beforeLine + '):\n\t\'' + lineBeforeText + '\'');
+
+		const STRUCT_KEY: string = '@' + commentKeyWords[CommentkeyWords.STRUCT].trim();
+		const UNION_KEY: string = '@' + commentKeyWords[CommentkeyWords.UNION].trim();
+		const FUNC_KEY: string = '@' + commentKeyWords[CommentkeyWords.FUNC].trim();
+		const VAR_KEY: string = '@' + commentKeyWords[CommentkeyWords.VARIABLE].trim();
+		const FIELD_KEY: string = '@' + commentKeyWords[CommentkeyWords.FIELD].trim();
+		const PARAM_KEY: string = '@' + commentKeyWords[CommentkeyWords.PARAM].trim();
+
+		// if line before is a complex then include the field keyword
+		if (lineBeforeText.includes(STRUCT_KEY) || lineBeforeText.includes(UNION_KEY)) {
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FIELD]));
+		}
+		// if line before is a function then include the param keyword
+		else if (lineBeforeText.includes(FUNC_KEY)) {
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.PARAM]));
+		}
+		// if line before is a variable, field or parameter include the type keyword
+		else if (lineBeforeText.includes(VAR_KEY) || lineBeforeText.includes(FIELD_KEY) || lineBeforeText.includes(PARAM_KEY)) {
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.TYPE]));
+		}
+		// else we include the main keywords
+		else {
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.VARIABLE]));
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.ENUM]));
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.STRUCT]));
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.UNION]));
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FUNC]));
+			items.push(new vscode.CompletionItem(commentKeyWords[CommentkeyWords.FILE]));
+		}
+		return items;
+	}
+
+	resolveCompletionItem?(item: vscode.CompletionItem, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem> {
+		return null;
 	}
 }
